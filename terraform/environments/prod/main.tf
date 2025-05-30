4df107a0de0bbffbbe7281d69e9e8912450d2ae6
Oluty-1 <tayoefunshile@gmail.com>
2025-05-30 18:30:55 +0100

    frontend configured

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -1,8 +1,8 @@
 module "shared" {
-  source         = "../../modules/shared"
-  environment    = var.environment
-  region         = var.region
-  alarm_email    = var.alarm_email
+  source          = "../../modules/shared"
+  environment     = var.environment
+  region          = var.region
+  alarm_email     = var.alarm_email
   integration_ids = [
     module.calculate_compatibility.integration_id,
     module.get_compatibility.integration_id,
@@ -12,105 +12,105 @@ module "shared" {
 }
 
 module "calculate_compatibility" {
-  source                 = "../../modules/lambda"
-  function_name          = "CalculateCompatibilityFunction"
-  environment            = var.environment
-  region                 = var.region
-  region_api_id          = var.region
-  account_id             = "302225372317"
-  s3_bucket              = "compatibility-bucket-test"
-  s3_key                 = "functions/calculateCompatibility/function.zip"
-  runtime                = "nodejs22.x"
-  timeout                = 120
-  sqs_queue_url          = module.shared.queue_url
-  sqs_queue_arn          = module.shared.queue_arn
-  api_gateway_id         = module.shared.api_gateway_id
-  api_gateway_resource_id    = module.shared.compatibility_resource_id
-  api_gateway_http_method     = "POST"
-  api_resource_path           = "/compatibility"
-  enable_sqs             = false
-  enable_api_gateway     = true
+  source                  = "../../modules/lambda"
+  function_name           = "CalculateCompatibilityFunction"
+  environment             = var.environment
+  region                  = var.region
+  region_api_id           = var.region
+  account_id              = "302225372317"
+  s3_bucket               = "compatibility-bucket-test"
+  s3_key                  = "functions/calculateCompatibility/function.zip"
+  runtime                 = "nodejs22.x"
+  timeout                 = 10
+  sqs_queue_url           = module.shared.queue_url
+  sqs_queue_arn           = module.shared.queue_arn
+  api_gateway_id          = module.shared.api_gateway_id
+  api_gateway_resource_id = module.shared.compatibility_resource_id
+  api_gateway_http_method = "POST"
+  api_resource_path       = "/compatibility"
+  enable_sqs              = false
+  enable_api_gateway      = true
 }
 
 module "process_compatibility" {
-  source            = "../../modules/lambda"
-  function_name          = "ProcessCompatibilityFunction"
-  environment            = var.environment
-  region                 = var.region
-  region_api_id          = var.region
-  account_id             = "302225372317"
-  s3_bucket              = "compatibility-bucket-test"
-  s3_key                 = "functions/processCompatibility/function.zip"
-  runtime                = "nodejs22.x"
-  timeout                = 120
-  sqs_queue_url          = module.shared.queue_url
-  sqs_queue_arn          = module.shared.queue_arn
-  dynamodb_table_name    = module.shared.table_name
-  api_gateway_id         = ""
+  source                  = "../../modules/lambda"
+  function_name           = "ProcessCompatibilityFunction"
+  environment             = var.environment
+  region                  = var.region
+  region_api_id           = var.region
+  account_id              = "302225372317"
+  s3_bucket               = "compatibility-bucket-test"
+  s3_key                  = "functions/processCompatibility/function.zip"
+  runtime                 = "nodejs22.x"
+  timeout                 = 10
+  sqs_queue_url           = module.shared.queue_url
+  sqs_queue_arn           = module.shared.queue_arn
+  dynamodb_table_name     = module.shared.table_name
+  api_gateway_id          = ""
   api_gateway_resource_id = ""
   api_gateway_http_method = ""
   api_resource_path       = ""
-  enable_sqs             = true
-  enable_api_gateway     = false
+  enable_sqs              = true
+  enable_api_gateway      = false
 }
 
 module "get_compatibility" {
-  source                 = "../../modules/lambda"
-  function_name          = "GetCompatibilityFunction"
-  environment            = var.environment
-  region                 = var.region
-  region_api_id          = var.region
-  account_id             = "302225372317"
-  s3_bucket              = "compatibility-bucket-test"
-  s3_key                 = "functions/getCompatibility/function.zip"
-  runtime                = "nodejs22.x"
-  timeout                = 120
-  dynamodb_table_name    = module.shared.table_name
-  dynamodb_read_only     = true
-  api_gateway_id         = module.shared.api_gateway_id
+  source                  = "../../modules/lambda"
+  function_name           = "GetCompatibilityFunction"
+  environment             = var.environment
+  region                  = var.region
+  region_api_id           = var.region
+  account_id              = "302225372317"
+  s3_bucket               = "compatibility-bucket-test"
+  s3_key                  = "functions/getCompatibility/function.zip"
+  runtime                 = "nodejs22.x"
+  timeout                 = 10
+  dynamodb_table_name     = module.shared.table_name
+  dynamodb_read_only      = true
+  api_gateway_id          = module.shared.api_gateway_id
   api_gateway_resource_id = module.shared.compatibility_id_resource_id
-  api_gateway_http_method     = "GET"
-  api_resource_path           = "/compatibility/{id}"
-  enable_sqs             = false
-  enable_api_gateway     = true
+  api_gateway_http_method = "GET"
+  api_resource_path       = "/compatibility/{id}"
+  enable_sqs              = false
+  enable_api_gateway      = true
 }
 
 module "update_compatibility" {
-  source                 = "../../modules/lambda"
-  function_name          = "UpdateCompatibilityFunction"
-  environment            = var.environment
-  region                 = var.region
-  region_api_id          = var.region
-  account_id             = "302225372317"
-  s3_bucket              = "compatibility-bucket-test"
-  s3_key                 = "functions/updateCompatibility/function.zip"
-  runtime                = "nodejs22.x"
-  timeout                = 120
-  dynamodb_table_name    = module.shared.table_name
-  api_gateway_id         = module.shared.api_gateway_id
+  source                  = "../../modules/lambda"
+  function_name           = "UpdateCompatibilityFunction"
+  environment             = var.environment
+  region                  = var.region
+  region_api_id           = var.region
+  account_id              = "302225372317"
+  s3_bucket               = "compatibility-bucket-test"
+  s3_key                  = "functions/updateCompatibility/function.zip"
+  runtime                 = "nodejs22.x"
+  timeout                 = 10
+  dynamodb_table_name     = module.shared.table_name
+  api_gateway_id          = module.shared.api_gateway_id
   api_gateway_resource_id = module.shared.compatibility_id_resource_id
-  api_gateway_http_method   = "PUT"
-  api_resource_path         = "/required"
-  enable_sqs             = false
-  enable_api_gateway     = true
+  api_gateway_http_method = "PUT"
+  api_resource_path       = "/compatibility/{id}"
+  enable_sqs              = false
+  enable_api_gateway      = true
 }
 
 module "delete_compatibility" {
-  source                 = "../../modules/lambda"
-  function_name          = "DeleteCompatibilityFunction"
-  environment            = var.environment
-  region                 = var.region
-  region_api_id          = var.region
-  account_id             = "302225372317"
-  s3_bucket              = "compatibility-bucket-test"
-  s3_key                 = "functions/deleteCompatibility/function.zip"
-  runtime                = "nodejs22.x"
-  timeout                = 120
-  dynamodb_table_name    = module.shared.table_name
-  api_gateway_id         = module.shared.api_gateway_id
+  source                  = "../../modules/lambda"
+  function_name           = "DeleteCompatibilityFunction"
+  environment             = var.environment
+  region                  = var.region
+  region_api_id           = var.region
+  account_id              = "302225372317"
+  s3_bucket               = "compatibility-bucket-test"
+  s3_key                  = "functions/deleteCompatibility/function.zip"
+  runtime                 = "nodejs22.x"
+  timeout                 = 10
+  dynamodb_table_name     = module.shared.table_name
+  api_gateway_id          = module.shared.api_gateway_id
   api_gateway_resource_id = module.shared.compatibility_id_resource_id
-  api_gateway_http_method     = "DELETE"
-  api_resource_path           = "/compatibility/{id}"
-  enable_sqs             = false
-  enable_api_gateway     = true
+  api_gateway_http_method = "DELETE"
+  api_resource_path       = "/compatibility/{id}"
+  enable_sqs              = false
+  enable_api_gateway      = true
 }
--- terraform/modules/shared/api.tf
+++ terraform/modules/shared/api.tf
@@ -15,44 +15,107 @@ resource "aws_api_gateway_resource" "compatibility_id_resource" {
   path_part   = "{id}"
 }
 
-resource "aws_api_gateway_method" "calculate_compatibility_post" {
+locals {
+  methods = {
+    "POST_compatibility" = {
+      http_method = "POST"
+      resource_id = aws_api_gateway_resource.compatibility_resource.id
+      is_options  = false
+    }
+    "GET_compatibility_id" = {
+      http_method = "GET"
+      resource_id = aws_api_gateway_resource.compatibility_id_resource.id
+      is_options  = false
+    }
+    "PUT_compatibility_id" = {
+      http_method = "PUT"
+      resource_id = aws_api_gateway_resource.compatibility_id_resource.id
+      is_options  = false
+    }
+    "DELETE_compatibility_id" = {
+      http_method = "DELETE"
+      resource_id = aws_api_gateway_resource.compatibility_id_resource.id
+      is_options  = false
+    }
+    "OPTIONS_compatibility" = {
+      http_method = "OPTIONS"
+      resource_id = aws_api_gateway_resource.compatibility_resource.id
+      is_options  = true
+    }
+    "OPTIONS_compatibility_id" = {
+      http_method = "OPTIONS"
+      resource_id = aws_api_gateway_resource.compatibility_id_resource.id
+      is_options  = true
+    }
+  }
+}
+
+resource "aws_api_gateway_method" "method" {
+  for_each      = local.methods
   rest_api_id   = aws_api_gateway_rest_api.compatibility_api.id
-  resource_id   = aws_api_gateway_resource.compatibility_resource.id
-  http_method   = "POST"
+  resource_id   = each.value.resource_id
+  http_method   = each.value.http_method
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "get_compatibility_get" {
-  rest_api_id   = aws_api_gateway_rest_api.compatibility_api.id
-  resource_id   = aws_api_gateway_resource.compatibility_id_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
+resource "aws_api_gateway_integration" "options_integration" {
+  for_each = { for k, v in local.methods : k => v if v.is_options }
+  rest_api_id             = aws_api_gateway_rest_api.compatibility_api.id
+  resource_id             = each.value.resource_id
+  http_method             = each.value.http_method
+  type                    = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+  depends_on = [aws_api_gateway_method.method]
 }
 
-resource "aws_api_gateway_method" "update_compatibility_put" {
-  rest_api_id   = aws_api_gateway_rest_api.compatibility_api.id
-  resource_id   = aws_api_gateway_resource.compatibility_id_resource.id
-  http_method   = "PUT"
-  authorization = "NONE"
+resource "aws_api_gateway_method_response" "response_200" {
+  for_each    = local.methods
+  rest_api_id = aws_api_gateway_rest_api.compatibility_api.id
+  resource_id = each.value.resource_id
+  http_method = each.value.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Methods" = each.value.is_options
+    "method.response.header.Access-Control-Allow-Headers" = each.value.is_options
+  }
+  response_models = each.value.is_options ? { "application/json" = "Empty" } : {}
+  depends_on = [aws_api_gateway_method.method] 
 }
 
-resource "aws_api_gateway_method" "delete_compatibility_delete" {
-  rest_api_id   = aws_api_gateway_rest_api.compatibility_api.id
-  resource_id   = aws_api_gateway_resource.compatibility_id_resource.id
-  http_method   = "DELETE"
-  authorization = "NONE"
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  for_each = { for k, v in local.methods : k => v if v.is_options }
+  rest_api_id = aws_api_gateway_rest_api.compatibility_api.id
+  resource_id = each.value.resource_id
+  http_method = each.value.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization,X-Amz-Date'"
+  }
+  depends_on = [
+    aws_api_gateway_method_response.response_200,
+    aws_api_gateway_integration.options_integration
+  ]
 }
 
 resource "aws_api_gateway_deployment" "compatibility_api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.compatibility_api.id
   triggers = {
     redeployment = sha1(jsonencode([
-      aws_api_gateway_method.calculate_compatibility_post,
-      aws_api_gateway_method.get_compatibility_get,
-      aws_api_gateway_method.update_compatibility_put,
-      aws_api_gateway_method.delete_compatibility_delete,
+      local.methods,
       var.integration_ids
     ]))
+  }
+  depends_on = [
+    aws_api_gateway_method.method,
+    aws_api_gateway_integration.options_integration
+  ]
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -60,4 +123,8 @@ resource "aws_api_gateway_stage" "compatibility_api_stage" {
   rest_api_id   = aws_api_gateway_rest_api.compatibility_api.id
   deployment_id = aws_api_gateway_deployment.compatibility_api_deployment.id
   stage_name    = var.environment
+  depends_on    = [aws_api_gateway_deployment.compatibility_api_deployment]
+  lifecycle {
+    create_before_destroy = true
+  }
 }
